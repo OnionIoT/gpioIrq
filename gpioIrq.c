f1a903f6481f2d21d0b2a64d649f1e7fc6f918a3
Lazar <lazardemin@gmail.com>
2018-03-03 23:41:14 -0500

    changed edge to both

--- gpioIrq.c
+++ gpioIrq.c
@@ -209,7 +209,7 @@ int main(int argc, char **argv, char **envp)
 
 	gpio_export(gpio);
 	gpio_set_dir(gpio, 0);
-	gpio_set_edge(gpio, "rising");
+	gpio_set_edge(gpio, "both");
 	gpio_fd = gpio_fd_open(gpio);
 
 	timeout = POLL_TIMEOUT;
